f76af1b81ef1a50148bc2db9d1c38ba8dd97d279
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-30 22:41:13 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function divide(){
+   let num1=2000;
+   let num2=100;
+   let result= num1/num2;
+ return  result;
+}
+divide();
+
+const square = x => x*x
+let x=2;
+square(x);
+
+const add =(a,b) => (a+b)
+let a=3;
+let b=4;
+add(a,b);
+
+/*const divide = result => {
+   let num1=2000;
+   let num2=100;
+   result= num1/num2;
+}*/
